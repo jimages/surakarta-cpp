9ca1b423a935670fac478cb24d1dcc26fe168de7
Zachary Wang <jimages123@gmail.com>
2020-06-16 00:05:08 +0800

    添加了libck作为库使用

---
+++ libck.cmake
@@ -0,0 +1,22 @@
+#libck
+include(${CMAKE_ROOT}/Modules/ExternalProject.cmake)
+set(LIBCK_DIR ${CMAKE_CURRENT_SOURCE_DIR}/libck)
+set(LIBCK_BIN ${CMAKE_CURRENT_BINARY_DIR}/libck)
+set(LIBCK_STATIC_LIB ${LIBCK_BIN}/lib/libck.a)
+set(LIBCK_INCLUDES ${LIBCK_BIN}/include)
+file(MAKE_DIRECTORY ${LIBCK_INCLUDES})
+
+ExternalProject_Add(
+    libck
+    PREFIX ${LIBCK_BIN}
+    SOURCE_DIR ${LIBCK_DIR}
+    CONFIGURE_COMMAND ${LIBCK_DIR}/configure --prefix=${LIBCK_BIN} --enable-rtm
+    BUILD_COMMAND make
+    INSTALL_COMMAND make install
+    BUILD_BYPRODUCTS ${LIBCK_STATIC_LIB}
+)
+add_library(ck STATIC IMPORTED GLOBAL)
+
+add_dependencies(ck libck)
+set_target_properties(ck PROPERTIES IMPORTED_LOCATION ${LIBCK_STATIC_LIB})
+set_target_properties(ck PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${LIBCK_INCLUDES})
